c5144de67a057f74e10b5e634102107327343741
Jacob Sklar <jsklar.js@gmail.com>
2017-05-08 14:58:12 -0700

    added test for map

--- test/map.test.js
+++ test/map.test.js
@@ -1,9 +1,20 @@
 _ = require('../underbar');
 
 describe('map()', () => {
-  it('maps every numbers in an array of numbers to their square', () => {
+  it('maps every number in an array of numbers to their square', () => {
     const arr = [1, 2, 3, 4, 5];
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
+  });
+
+  it('maps every number in an array-like object of numbers to their square', () => {
+    const arrayLikeObj = {
+      length: 3,
+      0: 1,
+      1: 2,
+      2: 3
+    };
+    const mappedArr = _.map(arrayLikeObj, (el) => el * el);
+    expect(mappedArr).toEqual([1, 4, 9]);
   });
 });

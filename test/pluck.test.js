6835613a4786ad8500b16e28230d5f891fa1b18e
Jacob Sklar <jsklar.js@gmail.com>
2017-05-09 13:01:06 -0700

    added pluck test

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,15 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  it('returns an array of ages and undefineds, given an array of people objects, only some of which have ages', () => {
+    const people = [
+      { name: 'Harriet', age: 12},
+      { name: 'Lazarus'},
+      { name: 'Bethany', age: 14},
+      { nmae: 'Sally'}
+    ];
+    const result = _.pluck(people, 'age');
+    expect(result).toEqual([12, undefined, 14, undefined]);
+  });
+
 });

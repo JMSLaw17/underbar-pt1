7664ccf6c8a543735ff41fa04e7a2e93e9a013f9
Jacob Sklar <jsklar.js@gmail.com>
2017-05-09 13:53:17 -0700

    added test for reject

--- test/reject.test.js
+++ test/reject.test.js
@@ -18,4 +18,9 @@ describe('reject()', () => {
     const orderItems = _.reject(order, (value) => value === null);
     expect(orderItems).toEqual(['burger', 'ketchup', 'cookie']);
   });
+
+  it('returns the same array when no values pass the test', () => {
+    const nums = [1, 3, 5, 7, 9];
+    expect(_.reject(nums, num => num % 2 === 0)).toEqual([1, 3, 5, 7, 9]);
+  });
 });

8f5acd47672a2a68a72d1e05e6639471c1ad9913
Jacob Sklar <jsklar.js@gmail.com>
2017-05-09 14:04:49 -0700

    added test for uniq

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,14 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('when given an object, returns an array with each unique value', () => {
+    const points = {
+      gameOne: 5,
+      gameTwo: 7,
+      gameThree: 7,
+      gameFour: 5
+      };
+    expect(_.uniq(points)).toEqual([5, 7]);
+  });
 });
